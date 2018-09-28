708e64ba0ce927d43515c2604af9c3aa3ad7eddd
Eddy.Kim <sieunkr@gmail.com>
2018-09-28 17:24:17 +0900

    item 14

---
+++ effective-java-second/item14/src/Coffee.java
@@ -0,0 +1,21 @@
+public class Coffee {
+
+    private String name;
+    private double y;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public double getY() {
+        return y;
+    }
+
+    public void setY(double y) {
+        this.y = y;
+    }
+}
