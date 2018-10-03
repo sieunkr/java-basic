8e1d98004bfaa4faf2c559c85d91eced45296453
sieunkim <sieunkr@gmail.com>
2018-10-03 19:19:23 +0900

    블록체인 테스트 코드

---
+++ blockchain/src/Block.java
@@ -0,0 +1,47 @@
+import java.util.Date;
+
+public class Block {
+
+    public String hash;
+    public String previousHash;
+    private String data; //our data will be a simple message.
+    private long timeStamp; //as number of milliseconds since 1/1/1970.
+    private int nonce;
+
+    //Block Constructor.
+    public Block(String data,String previousHash ) {
+        this.data = data;
+        this.previousHash = previousHash;
+        this.timeStamp = new Date().getTime();
+        this.hash = calculateHash(); //Making sure we do this after we set the other values.
+    }
+
+    //Calculate new hash based on blocks contents
+    public String calculateHash() {
+        String calculatedhash = StringUtil.applySha256(
+                previousHash +
+                        Long.toString(timeStamp) +
+                        Integer.toString(nonce) +
+                        data
+        );
+        return calculatedhash;
+    }
+
+    public void mineBlock(int difficulty) {
+        //String target = new String(new char[difficulty]).replace('\0', '0'); //Create a string with difficulty * "0"
+
+        //TEST
+        String target = "91e";
+
+        System.out.println("target : " + target);
+
+        while(!hash.substring( 0, 3).equals(target)) {
+            nonce ++;
+            hash = calculateHash();
+
+            //System.out.println(hash.substring( 0, 5));
+        }
+
+        System.out.println("Block Mined!!! : " + hash);
+    }
+}
